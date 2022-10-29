c9d434e82b030b99e19cbed2205ea0084aa060e6
Pavel Kurek <kurp03@vse.cz>
2022-10-30 00:41:38 +0200

    big change

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module cz.vse.kurp03.adventura {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens cz.vse.kurp03.adventura to javafx.fxml;
-    exports cz.vse.kurp03.adventura;
-}
